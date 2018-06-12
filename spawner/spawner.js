15fbe4ccc87b49040a7f6d3535db41f1218d87de
Sean Landis <sean.a.landis@gmail.com>
2018-06-11 20:13:19 -0400

    Adding ember days handling

--- spawner/spawner.js
+++ spawner/spawner.js
@@ -1,12 +1,16 @@
+const getOneFromList = require('../redis/getOneFromList')
+const getSpawnList = require('./components/getSpawnList')
 const spawnCollectible = require('./spawnCollectible')
 const spawnLocation = require('./spawnLocation')
 const spawnSpirit = require('./spawnSpirit')
-const getSpawnList = require('./components/getSpawnList')
 
 async function spawner(message) {
   try {
     if (message.event === 'creation') {
-      await spawnLocation(message.latitude, message.longitude)
+      const emberDays = await getOneFromList('constants', 'emberDays')
+      if (!emberDays) {
+        await spawnLocation(message.latitude, message.longitude)
+      }
     }
     else if (message.event === 'move') {
       const spawnList = await getSpawnList(message.latitude, message.longitude)
