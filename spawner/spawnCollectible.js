8993d0fe2982250f1667f1917d091861bf050b1c
Sean Landis <sean.a.landis@gmail.com>
2018-08-20 16:49:32 -0400

    Formatting

--- spawner/spawnCollectible.js
+++ spawner/spawnCollectible.js
@@ -44,21 +44,28 @@ module.exports = (latitude, longitude, spawnList) => {
       for (let i = 0; i < types.length; i++) {
         const rarityFour = collectibles
           .filter(collectible => collectible.rarity === 4)
+
         if (rarityFour.length >= collectibleDensity[3]) {
           continue
         }
 
-        const rarityThree = collectibles.filter(collectible => collectible.rarity === 3)
+        const rarityThree = collectibles
+          .filter(collectible => collectible.rarity === 3)
+
         if (rarityThree.length >= collectibleDensity[2]) {
           continue
         }
 
-        const rarityTwo = collectibles.filter(collectible => collectible.rarity === 2)
+        const rarityTwo = collectibles
+          .filter(collectible => collectible.rarity === 2)
+
         if (rarityTwo.length >= collectibleDensity[1]) {
           continue
         }
 
-        const rarityOne = collectibles.filter(collectible => collectible.rarity === 1)
+        const rarityOne = collectibles
+          .filter(collectible => collectible.rarity === 1)
+
         if (rarityOne.length >= collectibleDensity[0]) {
           continue
         }
