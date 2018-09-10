e72f5a582c7a5a3231bc21df2c54f16f8a94f9b8
Sean Landis <sean.a.landis@gmail.com>
2018-09-10 14:51:40 -0400

    Converted to match Game/manager

--- utils/informNearbyPlayers.js
+++ utils/informNearbyPlayers.js
@@ -1,33 +1,56 @@
+const getFieldsFromHash = require('../redis/getFieldsFromHash')
 const getOneFromHash = require('../redis/getOneFromHash')
 const getOneFromList = require('../redis/getOneFromList')
 const getNearbyFromGeohash = require('../redis/getNearbyFromGeohash')
 const informPlayers = require('./informPlayers')
 
-module.exports = (latitude, longitude, message, exclude = []) => {
+module.exports = (entity, message, trueSightCheck = 0, exclude = []) => {
   return new Promise(async (resolve, reject) => {
     try {
-      if (
-        typeof latitude !== 'number' && typeof longitude !== 'number' ||
-        isNaN(latitude) || isNaN(longitude)
-      ) {
-        throw new Error('Invalid coords: ' + latitude + ', ' + longitude)
+      let playersToInform
+      if (entity.location) {
+        const occupants = await getOneFromHash(entity.location, 'occupants')
+
+        playersToInform = await Promise.all(
+          Object.keys(occupants)
+            .filter(instance => !exclude.includes(instance))
+            .map(instance => getFieldsFromHash(instance, ['player', 'conditions']))
+        )
+      }
+      else {
+        const displayRadius = await getOneFromList('constants', 'displayRadius')
+
+        const nearCharacters = await getNearbyFromGeohash(
+          'characters',
+          entity.fuzzyLatitude || entity.latitude,
+          entity.fuzzyLongitude || entity.longitude,
+          displayRadius
+        )
+
+        playersToInform =
+          await Promise.all(nearCharacters
+            .filter(instance => !exclude.includes(instance))
+            .map(instance => getFieldsFromHash(instance, ['player', 'conditions']))
+          )
       }
 
-      const displayRadius =
-        await getOneFromList('constants', 'displayRadius')
-
-      const nearCharacters = await getNearbyFromGeohash(
-        'characters',
-        latitude,
-        longitude,
-        displayRadius
-      )
-
-      const playersToInform =
-        await Promise.all(nearCharacters
-          .filter(instance => !exclude.includes(instance))
-          .map(instance => getOneFromHash(instance, 'player'))
-        )
+      if (trueSightCheck === 1) {
+        playersToInform = playersToInform
+          .filter(player =>
+              Object.values(player[1]).map(condition => condition.status).includes('truesight')
+            )
+            .map(player => player[0])
+      }
+      else if (trueSightCheck === 2) {
+        playersToInform = playersToInform
+          .filter(player =>
+              !Object.values(player[1]).map(condition => condition.status).includes('truesight')
+            )
+            .map(player => player[0])
+      }
+      else {
+        playersToInform = playersToInform.map(player => player[0])
+      }
 
       await informPlayers(playersToInform, message)
       resolve(true)
