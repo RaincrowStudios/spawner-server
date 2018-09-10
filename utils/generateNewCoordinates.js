204f5f98450701adad524617ee26b4a692fe29e3
Sean Landis <sean.a.landis@gmail.com>
2018-09-10 14:51:04 -0400

    Need to convert to degrees before modulo

--- utils/generateNewCoordinates.js
+++ utils/generateNewCoordinates.js
@@ -27,10 +27,10 @@ module.exports = (latitudeDegree, longitudeDegree, min, max, direction) => {
     Math.sin(bearing) * Math.sin(distance/earthRadius) * Math.cos(latitude),
     Math.cos(distance/earthRadius) - Math.sin(latitude) * Math.sin(newLatitude))
 
-  newLongitude = (newLongitude + 540) % 360 - 180
-
   newLatitude = radiansToDegrees(newLatitude)
   newLongitude = radiansToDegrees(newLongitude)
+
+  newLongitude = (newLongitude + 540) % 360 - 180
 
   if (newLatitude < -85.05112878 || newLatitude > 85.05112878) {
     newLatitude = (Math.sign(newLatitude) * 85.05)
