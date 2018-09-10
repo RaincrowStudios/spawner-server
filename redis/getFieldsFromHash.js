a8c8605c1140c91d759a757c34fc4d920cc776c8
Sean Landis <sean.a.landis@gmail.com>
2018-09-10 14:51:48 -0400

    Needed for utils

---
+++ redis/getFieldsFromHash.js
@@ -0,0 +1,26 @@
+const selectRedisClient = require('./selectRedisClient')
+
+module.exports = (instance, fields) => {
+  return new Promise((resolve, reject) => {
+    try {
+      if (!instance || typeof instance !== 'string') {
+        throw new Error('Invalid instance: ' + instance)
+      }
+      else if (!fields || !Array.isArray(fields)) {
+        throw new Error('Invalid fields: ' + fields)
+      }
+
+      const client = selectRedisClient(instance)
+
+      client.hmget([instance, ...fields], (err, results) => {
+        if (err) {
+          throw new Error('5300')
+        }
+        resolve(results.map(result => JSON.parse(result)))
+      })
+    }
+    catch (err) {
+      reject(err)
+    }
+  })
+}
